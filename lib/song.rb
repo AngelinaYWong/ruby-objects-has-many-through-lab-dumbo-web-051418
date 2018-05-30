031ba39175829e4a7e2f2d8c3040e11061fd6e10
Angelina Wong <angelina.w25@gmail.com>
2018-05-30 01:46:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :genre, :artist
   
   @@all = []
-  def inititialize(name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre =  genre
